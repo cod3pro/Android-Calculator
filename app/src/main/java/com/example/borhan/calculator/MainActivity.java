85fb5cb2b4cae1043f3aa4ecbf183f92abc93308
Borhan <net2net4all@gmail.com>
2017-06-11 19:21:40 -0700

    GUI completed

--- app/src/main/java/com/example/borhan/calculator/MainActivity.java
+++ app/src/main/java/com/example/borhan/calculator/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.borhan.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
+    public void doCalculate(View view){
+        tx = (TextView) findViewById(R.id.textView);
+        tx.setText(getResources().getResourceEntryName(view.getId()));
+    }
+
 }
